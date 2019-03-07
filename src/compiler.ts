6e4447fa1491ba642897cecfe9fc78b4b760a726
andrusyshyny <yurij.andrusyshyn@gmail.com>
2019-03-07 13:04:46 +0200

    query compiler

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,14 +1,30 @@
 import { TableMetaProvider, WhereBuilder, OrderBuilder, and, or, asc, desc, IOperandable } from '@viatsyshyn/ts-orm';
-import { IWhereInput, IOrderBy, AND, OR, ASC, DESC, Operation } from '@viatsyshyn/ts-rest-query';
+import { IWhereInput, IOrderBy, AND, OR, ASC, DESC, Operation, OP_EQ, OP_NEQ, OP_GT, OP_GTE, OP_LT, OP_LTE, OP_SEARSH, OP_INCLUDES } from '@viatsyshyn/ts-rest-query';
 
-export function compileWhereFor<T extends TableMetaProvider<InstanceType<T>>>(Model: T, query: IWhereInput): WhereBuilder<InstanceType<T>> {
+export function compileWhereFor<T extends TableMetaProvider<InstanceType<T>>>(Model: T, query?: IWhereInput): WhereBuilder<InstanceType<T>> | undefined {
+  
+  if(!query) {
+    return undefined;
+  }
+
   const filters = {
     [AND]: and,
     [OR]: or,
   };
-  return (props) => filters[query.op](Object.keys(query.filter).map(prop => {
+  const operationsMap = {
+    [OP_EQ]: 'eq',
+    [OP_NEQ]: 'neq',
+    [OP_GT]: 'gt',
+    [OP_GTE]: 'gte',
+    [OP_LT]: 'lt',
+    [OP_LTE]: 'lte',
+    [OP_SEARSH]: 'like',
+    [OP_INCLUDES]: 'in'
+  }
+  return (props) => filters[query.op](...Object.keys(query.filter).map(prop => {
     const conditions = query.filter[prop];
-    let res = Object.keys(conditions).reduce((a: IOperandable<T[any]>, op: Operation) => a[op](conditions[op]), props[prop]);
+    let res = Object.keys(conditions)
+      .reduce((a: IOperandable<T[any]>, op: Operation) => a[operationsMap[op]](conditions[op]), props[prop]);
     return res;
   }) as any);
 }
--- src/query-parser-middleware.ts
+++ src/query-parser-middleware.ts
@@ -1,6 +1,6 @@
 import { compileOrderFor, compileWhereFor } from './compiler';
 import { TableMetaProvider } from '@viatsyshyn/ts-orm';
-import { ISelectQueryInput, parser } from '@viatsyshyn/ts-rest-query';
+import { ISelectQueryInput, parser, COUNT_COLUMN, OR } from '@viatsyshyn/ts-rest-query';
 
 import { IMiddleware, IHttpRequest } from '@viatsyshyn/ts-rest';
 import { DecoratorUtils } from './decorator-utils';
@@ -11,11 +11,20 @@ export function QueryParserMiddlewareFor<T extends TableMetaProvider<InstanceTyp
   let middleware = class implements IMiddleware  {
     public async handle(req: IHttpRequest): Promise<void> {
       const query: ISelectQueryInput = parser(req.query);
-      req.params.query = compileWhereFor(Model, query.where || {filter: {}, op: 'OR'});
+      const countOnly = !!query.columns && query.columns.length === 1 && query.columns[0] === COUNT_COLUMN;
+      req.params.query = compileWhereFor(Model, query.where);
       req.params.limit = query.limit;
       req.params.offset = query.offset;
       req.params.orderBy = compileOrderFor(Model, query.orderBy || {});
-      req.params.countOnly = query.columns && query.columns.length === 1 && query.columns[0] === 'count(*)'
+      req.params.countOnly = countOnly;
+
+      console.log('Params');
+      console.log('query', req.params.query);
+      console.log('orderBy', req.params.orderBy);
+      console.log('limit', req.params.limit);
+      console.log('offset', req.params.offset);
+      console.log('countOnly', req.params.countOnly);
+      console.log('============');
     }
   }
 
