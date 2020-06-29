5f494a5a8de4b7272ca3ce47937ecb58b25c7631
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2020-06-29 12:17:42 +0300

    v2

--- src/query-parser-middleware.ts
+++ src/query-parser-middleware.ts
@@ -2,20 +2,20 @@ import { compileOrderFor, compileWhereFor } from './compiler';
 import { TableMetaProvider } from '@viatsyshyn/ts-orm';
 import { parser } from '@viatsyshyn/ts-rest-query';
 
-import { IMiddleware, IHttpRequest } from '@viatsyshyn/ts-rest';
+import { IMiddleware } from '@viatsyshyn/ts-rest';
 import { D } from './decorator-utils';
 import { injectable } from 'inversify'
 
 export function QueryParserMiddlewareFor<T extends TableMetaProvider<InstanceType<T>>>(Model: T): Function {
 
   const Middleware = class implements IMiddleware  {
-    public async handle(req: IHttpRequest): Promise<void> {
-      const {query, offset, limit, countOnly, orderBy} = parser(req.query);
-      req.query.query = compileWhereFor(Model, query);
-      req.query.limit = limit;
-      req.query.offset = offset;
-      req.query.orderBy = compileOrderFor(Model, orderBy);
-      req.query.countOnly = countOnly;
+    public async handle({query: _}: any): Promise<void> {
+      const {query, offset, limit, countOnly, orderBy} = parser(_);
+      _.query = compileWhereFor(Model, query);
+      _.limit = limit;
+      _.offset = offset;
+      _.orderBy = compileOrderFor(Model, orderBy);
+      _.countOnly = countOnly;
     }
   };
 
